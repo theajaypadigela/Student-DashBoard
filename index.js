3c43fde7410481b7a005a07815eb6077eb97b864
theajaypadigela <theajaypadigela@gmail.com>
2025-03-03 01:58:13 +0530

    added pages other than Dashboard and Alumni Network

--- index.js
+++ index.js
@@ -55,7 +55,12 @@ app.get("/sidebar",(req,res)=>{
 });
 
 app.get("/page",(req,res)=>{
-    res.send("Welcome to the page");
+    if(req.isAuthenticated()){
+        // res.render("page.ejs");
+        res.render("sidebar.ejs");
+    }else{
+        res.redirect("/login");
+    }
 });
 
 app.get("/login",(req,res)=>{
@@ -75,6 +80,263 @@ app.post("/login",
 
 app.get("/fail",(req,res)=>{
     res.send("Failed to login");
+});
+
+app.get("/dashboard",(req,res)=>{
+    if(req.isAuthenticated()){
+        res.render("dashboard.ejs");
+    }else{
+        res.redirect("/login");
+    }
+});
+
+const attendanceData=[
+    {
+        subject:"Mathematics",
+        present:60,
+        absent:40,
+        total:100,
+        percentage: 60/100*100,
+        color:"#e91e63"
+    },
+    {
+        subject:"Physics",
+        present:80,
+        absent:20,
+        total:300,
+        percentage: 80/100*100,
+        color:"#ff9800"
+    },
+    {
+        subject:"Chemistry",
+        present:70,
+        absent:30,
+        total:100,
+        percentage: 70/100*100,
+        color:"#3f51b5"
+    },
+    {
+        subject:"Biology",
+        present:90,
+        absent:10,
+        total:100,
+        percentage: 90/100*100,
+        color:"#4caf50"
+    },
+    {
+        subject:"English",
+        present:65,
+        absent:35,
+        total:100,
+        percentage: 65/100*100,
+        color:"#9c27b0"
+    }   
+]
+
+app.get("/attendance",(req,res)=>{
+    if(req.isAuthenticated()){
+        res.render("attendance.ejs",{
+            attendanceData:attendanceData
+        });
+    }else{
+        res.redirect("/login");
+    }
+});
+
+app.get('/timetable', function(req, res) {
+    const subjectColors = {
+      "Math": "accent-pink-gradient",
+      "Science": "accent-orange-gradient",
+      "English": "accent-green-gradient",
+      "History": "accent-cyan-gradient",
+      "Geography": "accent-blue-gradient",
+      "Physics": "accent-purple-gradient",
+      "Chemistry": "accent-orange-gradient",
+      "Biology": "accent-green-gradient"
+    };
+
+    const timetable = {
+      "mon1": "Math", "mon3": "English", "mon4": "History",
+      "tue2": "Math", "tue4": "Geography",
+      "wed1": "Science", "wed3": "Physics", "wed4": "Chemistry",
+      "thu1": "Biology", "thu3": "English", "thu4": "History",
+      "fri2": "Math", "fri4": "Geography"
+    };
+
+    res.render('timetable.ejs', { 
+        name: "Guest", 
+        subjectColors: subjectColors,
+        timetable: timetable
+    });
+});
+
+const title=["Web Development","Data Structure","Operating System","Computer Network","Database Management System"];
+
+app.get("/study-material",(req,res)=>{
+    if(req.isAuthenticated()){
+        res.render("study-material.ejs",{
+            title:title
+        });
+    }else{
+        res.redirect("/login");
+    }
+});
+
+const libraryData=[
+    {
+        title:"Artificial Intelligence: A Modern Approach",
+        rack:"1",
+        books:5
+    },
+    {
+        title:"The Emotion Machine",
+        rack:"1",
+        books:3
+    },  
+    {
+        title:"Super Intelligence: Paths,Dangers,Strategies",
+        rack:"1",
+        books:2
+    },
+    {   
+        title:"The Emotion Machine",
+        rack:"1",
+        books:3
+    },
+    {
+        title:"The Emotion Machine",
+        rack:"1",
+        books:3
+    },  
+    {
+        title:"The Emotion Machine",
+        rack:"1",
+        books:3
+    },  
+    {
+        title:"The Emotion Machine",
+        rack:"1",
+        books:3
+    },    
+    {
+        title:"The Emotion Machine",
+        rack:"1",
+        books:3
+    },  
+    
+]
+
+app.get("/library",(req,res)=>{
+    if(req.isAuthenticated()){
+        res.render("library.ejs",{
+            libraryData:libraryData
+        });
+    }else{
+        res.redirect("/login");
+    }
+});
+
+const opportunitiesData=[
+    {
+        title:"Google Internship 2025",
+        startDate:"24 Mar 2025",
+        duration:"1 Month",
+        applyBy:"20 Mar 2025",
+        description:"Google looking for undergraduate students.",
+        skillsRequired:"C, C++, Java, Data Structures and Algorithms"
+    },
+    {
+        title:"Research Intern at MIT",
+        startDate:"31 April 2025",
+        duration:"3 Months",
+        applyBy:"15 April 2025",
+        description:"MIT looking for talented researchers.",
+        skillsRequired:"Machine Learning, Computer Vision"
+    },
+    {
+        title:"Google Internship 2025",
+        startDate:"24 Mar 2025",
+        duration:"1 Month",
+        applyBy:"20 Mar 2025",
+        description:"Google looking for undergraduate students.",
+        skillsRequired:"C, C++, Java, Data Structures and Algorithms"
+    },
+    {
+        title:"Research Intern at MIT",
+        startDate:"31 April 2025",
+        duration:"3 Months",
+        applyBy:"15 April 2025",
+        description:"MIT looking for talented researchers.",
+        skillsRequired:"Machine Learning, Computer Vision"
+    },
+    {
+        title:"Google Internship 2025",
+        startDate:"24 Mar 2025",
+        duration:"1 Month",
+        applyBy:"20 Mar 2025",
+        description:"MIT looking for talented researchers."
+    }
+]
+
+app.get("/opportunities",(req,res)=>{
+    if(req.isAuthenticated()){
+        res.render("opportunities.ejs",{
+            opportunitiesData:opportunitiesData
+        });
+    }else{
+        res.redirect("/login");
+    }
+});
+
+const date=new Date();
+const day=date.getDate();
+const month=date.getMonth();
+const year=date.getFullYear();
+
+const studentRequestData=[
+    {
+        choose:"Certificate",
+        title:"MEMO",
+        description:"Need certificate for applying scholarship",
+        status:"RESOLVED",
+        date:`${day}/${month}/${year}`
+    },
+    {
+        choose:"CHOOSE",
+        title:"TITLE",
+        description:"description is written here",
+        status:"INITIATED",
+        date:`${day}/${month}/${year}`
+    },
+    {
+        choose:"Certificate",
+        title:"MEMO",
+        description:"Need certificate for applying scholarship",
+        status:"RESOLVED",
+        date:`${day}/${month}/${year}`
+    }
+]
+
+app.get("/student-request",(req,res)=>{
+    if(req.isAuthenticated()){
+        res.render("student-request.ejs",{
+            studentRequestData:studentRequestData
+        });
+    }else{
+        res.redirect("/login");
+    }
+});
+
+app.get("/student-request/req_new",(req,res)=>{
+    res.render("req_new.ejs");
+});
+
+app.get("/complaints",(req,res)=>{
+    res.render("complaints.ejs");
+});
+
+app.get("/complaints/comp_new",(req,res)=>{
+    res.render("comp_new.ejs");
 });
 
 app.get(
